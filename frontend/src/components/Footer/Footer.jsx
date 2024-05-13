2477d16bdd2ed70e4c02ec43c6a7b47ccb215d2d
Narendra Maurya <amanlearntoenvent17@gmail.com>
2024-05-13 15:19:09 +0530

    changes the footer

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import './Footer.css'
 import { assets } from '../../assets/assets'
+import { Link } from 'react-router-dom'
 
 const Footer = () => {
     const currentYear = new Date().getFullYear()
@@ -11,7 +12,7 @@ const Footer = () => {
 
             <div className='footer-content-left'>
                 <img src={assets.logo} alt="" />
-                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Corporis nulla tenetur ratione porro harum dignissimos unde dolorem aperiam eveniet sit, sint modi et deserunt culpa, numquam veritatis reiciendis quibusdam dolor!</p>
+                <p>Our Web App, "Tomato," revolutionizes the way you experience food delivery. With a seamless interface, it allows you to explore a variety of restaurants, order your favorite meals, and have fun</p>
                 <div className="footer-social-icons">
                     <img src={assets.facebook_icon} alt="" />
                     <img src={assets.twitter_icon} alt="" />
@@ -22,7 +23,7 @@ const Footer = () => {
             <div className='footer-content-center'>
                 <h2>COMPANY</h2>
                 <ul>
-                    <li>Home</li>
+                    <li>Home</li> 
                     <li>About us</li>
                     <li>Delivery</li>
                     <li>Privacy policy</li>
